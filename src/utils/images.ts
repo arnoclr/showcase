b677bd4970472d03952a57e95566e6be98bbf68b
Arno Cellarier <git@arno.cl>
2024-03-10 11:10:29 +0100

    refactor: class lists

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,16 +1,10 @@
 export enum ImageSizePreset {
-  ThreePhonesOrTablet,
-  SinglePhone,
-  TabletAndPhone,
+  ThreePhonesOrTablet = "threePhonesOrTablet",
+  SinglePhone = "singlePhone",
+  TabletAndPhone = "tabletAndPhone",
 }
 
-export function getImageSize(preset: ImageSizePreset): string {
-  switch (preset) {
-    case ImageSizePreset.ThreePhonesOrTablet:
-      return "width: 60%; height: auto;";
-    case ImageSizePreset.SinglePhone:
-      return "width: auto; height: 80vh;";
-    case ImageSizePreset.TabletAndPhone:
-      return "width: 100%; height: auto;";
-  }
+export enum ImageColorScheme {
+  Light = "for-light-mode",
+  Dark = "for-dark-mode",
 }
